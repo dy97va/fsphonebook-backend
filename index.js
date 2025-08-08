50df0d157c4d2c0399268d2201869c124b6e70e7
valentin <dy97va@gmail.com>
2025-08-08 13:03:45 +0300

    backend connected to db

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
+const Person = require('./models/person')
+
 const app = express()
 app.use(express.static('dist'))
-
 app.use(express.json())
-
 morgan.token('content', (request) => JSON.stringify(request.body))
 app.use(morgan(':method :url :status :response-time ms :content'))
 
@@ -33,61 +34,85 @@ let persons = [
 
 app.get('/info', (request, response) => {
     const date = new Date()
-    response.send(`<div> Phonebook contains info for ${persons.length} people <div/> <div> ${date} <div/>` )
+    Person.find({}).then(persons => {
+        response.send(`<div> Phonebook contains info for ${persons.length} people <div/> <div> ${date} <div/>` )
+    })
 })
 
 app.get('/api/persons', (request, response)=>{
-    response.json(persons)
+    Person.find({}).then(persons => {
+        response.json(persons)
+    })
 })
 
-app.get('/api/persons/:id', (request, response) => {
-    const id = request.params.id
-    const person = persons.find(person => person.id === id)
-    if (person){
-        response.json(person)
-    } else {
-        response.status(404).end()
-    }
+app.get('/api/persons/:id', (request, response, next) => {
+    Person.findById(request.params.id).then(person => {
+        if ( person ) {
+            response.json(person)
+        } else {
+            response.status(404).end
+        }
+    })
+    .catch(error => next(error))
 })
 
-const generateId = () => {
-    const max = 100000
-    const min = 5
-    const minCeiled = Math.ceil(min);
-    const maxFloored = Math.floor(max);
-    return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled)
-}
-
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
     const body = request.body
 
-    if (!body.name || !body.number) {
-        return response.status(400).json({
-            error: 'number or name is missing'
-        })
-    }
+    // if (persons.find(person => person.name === body.name)){
+    //     return response.status(400).json({
+    //         error: 'name must be unique'
+    //     })
+    // }
 
-    if (persons.find(person => person.name === body.name)){
-        return response.status(400).json({
-            error: 'name must be unique'
-        })
-    }
-
-    const person = {
+    const person = new Person ({
         name: body.name,
         number: body.number,
-        id: generateId()
-    }
+    })
     
-    persons = persons.concat(person)
-    response.json(person)
+    person.save().then(savedPerson => {
+        response.json(savedPerson)
+    })
+    .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (request, response)=> {
-    const id = request.params.id
-    persons = persons.filter(person => person.id !== id)
-    response.status(204).end()
+app.put('/api/persons/:id', (request, response, next) => {
+    const {name, number } = request.body
+    Person.findById(request.params.id)
+    .then(person => {
+        if (!person ){ 
+            return response.status(404).end()
+        }
+        person.name = name
+        person.number = number
+
+        return person.save().then((updatedPerson) => {
+            response.json(updatedPerson)
+        })
+        .catch(error => next(error))
+    })
 })
+
+
+app.delete('/api/persons/:id', (request, response, next)=> {
+    Person.findByIdAndDelete(request.params.id).then(result => {
+        response.status(204).end()
+    })
+    .catch(error => next(error))
+})
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({error: 'malformatted id'})
+    } else if (error.name === 'ValidationError') {
+        return response.status(400).json({error: error.message})
+    }
+
+    next(error)
+}
+app.use(errorHandler)
 
 
 const PORT = process.env.PORT || 3001
